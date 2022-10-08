140c7ccbc29c3ee0f01370addaba2b34ce1dd9ec
cmikedev <carberrm@gmail.com>
2022-10-08 14:37:32 +0000

    Add increment and display score functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 
 function id(id) {
     return document.getElementById(id);
-}
+};
+
+let rockPic = '<span class="pic-choice"><i class="fa-regular fa-hand-back-fist"></i></span>';
+let paperPic = '<span class="pic-choice"><i class="fa-regular fa-hand"></i></span>';
+let scissorsPic = '<span class="pic-choice"><i class="fa-regular fa-hand-scissors"></i></span>';
 
 let playerChoice = "";
 let computerChoice = "";
@@ -12,58 +16,72 @@ let computerScore = 0;
 /*----------------------Populating Player Choice Image */
 
 id("rock-box").addEventListener("click", () => {
-    id("player-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand-back-fist"></i></span>'
+    id("player-choice").innerHTML = rockPic;
     playerChoice = "rock";
     createComputerChoice();
+    calcScores(playerChoice, computerChoice);
+    updateScores();
 });
 
 id("paper-box").addEventListener("click", () => {
-    id("player-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand"></i></span>'
+    id("player-choice").innerHTML = paperPic;
     playerChoice = "paper";
     createComputerChoice();
+    calcScores(playerChoice, computerChoice);
+    updateScores();
   });
 
 id("scissors-box").addEventListener("click", () => {
-    id("player-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand-scissors"></i></span>'
+    id("player-choice").innerHTML = scissorsPic;
     playerChoice = "scissors";
     createComputerChoice();
+    calcScores(playerChoice, computerChoice);
+    updateScores();
 });
 
-/*----------------------Populating Computer Choice Image */
+/*----------------------Populating Computer Choice Image & Calculating Scores */
 
 const choices = ["rock", "paper", "scissors"];
 
 function createComputerChoice() {
-    let result = choices[Math.floor(Math.random() * 3)];
-    computerChoice = result;
-
+    computerChoice = choices[Math.floor(Math.random() * 3)];
     if (playerChoice != "") {
         if (computerChoice === "rock") {
-            id("computer-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand-back-fist"></i></span>';
+            id("computer-choice").innerHTML = rockPic;
         } else if (computerChoice === "paper") {
-            id("computer-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand"></i></span>'
+            id("computer-choice").innerHTML = paperPic;
         } else {
-            id("player-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand-scissors"></i></span>'
+            id("computer-choice").innerHTML = scissorsPic;
         }
     }
 };
 
-/*let computerChoice = choices[createComputerChoice()];
+/*----------------------Incrementing the Scores */
 
-function createComputerChoice() {
-    let result = Math.floor(Math.random() * 3) + 1;
-    return result
-}
+function calcScores(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        playerScore += 0;
+        computerScore += 0;
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        computerScore ++;
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        playerScore ++;
+    }   else if (playerChoice === "paper" && computerChoice === "scissors") {
+        computerScore ++;
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        playerScore ++;
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        computerScore ++;
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        playerScore ++;
+    };
+};
 
-if (playerChoice != "") {
-    if (computerChoice === "rock") {
-        id("computer-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand-back-fist"></i></span>';
-    } else if (computerChoice === "paper") {
-        id("computer-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand"></i></span>'
-    } else {
-        id("player-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand-scissors"></i></span>'
-    }
-}*/
+/*----------------------Displaying the Scores */
 
-/*----------------------Incrementing Scores */
+function updateScores() {
+    id("player-score").innerText = playerScore;
+    id("computer-score").innerText = computerScore;
+};
 
+/*----------------------Clearing the Scores and Images */
