f59346372a0521a6bd11ad1688e9d8202428b296
cmikedev <carberrm@gmail.com>
2022-10-08 12:00:58 +0000

    create javascript to populate player choice image

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,34 @@ function id(id) {
     return document.getElementById(id);
 }
 
-const choices = ["rock", "paper", "scissors"]
+/*----------------------Populating Player Choice Image */
+
+let playerChoice = "";
+
+id("rock-box").addEventListener("click", () => {
+    id("player-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand-back-fist"></i></span>'
+    playerChoice = "rock"
+});
+
+id("paper-box").addEventListener("click", () => {
+    id("player-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand"></i></span>'
+    playerChoice = "paper"
+  });
+
+id("scissors-box").addEventListener("click", () => {
+    id("player-choice").innerHTML = '<span class="pic-choice"><i class="fa-regular fa-hand-scissors"></i></span>'
+
+    playerChoice = "scissors"
+});
+
+/*----------------------Populating Computer Choice Image */
+
+const choices = ["rock", "paper", "scissors"];
 
 let computerChoice = choices[createComputerChoice()];
-
-
 
 function createComputerChoice() {
     let result = Math.floor(Math.random() * 3) + 1;
     return result
 }
+
